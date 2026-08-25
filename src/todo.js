29f54be5a2a3c9007766cee97fea230d193b5191
nitheshraj <ipnitheshraj21@gmail.com>
2026-08-25 13:51:02 +0000

    added methods for creating todos, changing their priority and marking them as complete.

--- src/todo.js
+++ src/todo.js
@@ -8,5 +8,18 @@ class Todo{
         this.priority = priority;
         this.checkList = false;
     }
+
+    createTodo(title , description , dueDate , priority){
+        const todo = new Todo(title , description , dueDate , priority);
+    }
+
+    markComplete(todo){
+        todo.checkList = true;
+    }
+    
+    changePriority(todo){
+        todo.priority = "";
+    }
+
 }
 
